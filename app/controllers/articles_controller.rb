73595691f06376d993f59e4a7770afa6f1483211
Kevin Miller <scotty_terrific@yahoo.com>
2015-12-26 17:27:43 -0600

    Added routes

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,24 @@
 class ArticlesController < ApplicationController
   def index
+    @article = Article.all
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+
+    if @article.save
+      flash[:success] = 'Article has been created'
+    end
+    redirect_to articles_path
+  end
+
+  private
+  def article_params
+    params.require(:article).permit(:title, :body)
   end
 end
+

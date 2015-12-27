a6de7d52777c3032e5ea4b126c21ee3208e2d589
Kevin Miller <scotty_terrific@yahoo.com>
2015-12-27 10:25:21 -0600

    Refactored forms

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,6 @@
 class ArticlesController < ApplicationController
+  before_filter :set_article, only: [:show, :edit, :update, :destroy]
+
   def index
     @articles = Article.all
   end
@@ -20,15 +22,12 @@ class ArticlesController < ApplicationController
   end
 
   def show
-      @article = Article.find(params[:id])
   end
 
   def edit
-    @article = Article.find(params[:id])
   end
 
   def update
-    @article = Article.find(params[:id])
     if @article.update(article_params)
       flash[:success] = 'Article has been updated'
       redirect_to @article
@@ -36,21 +35,26 @@ class ArticlesController < ApplicationController
       flash.now[:danger] = 'Article has not been updated'
       render :edit
     end
+  end
 
-    def destroy
-      @article = Article.find(params[:id])
-      if @article.destroy
-        flash[:success] = 'Article has been deleted'
-        redirect_to articles_path
-      else
-        flash[:danger] = 'Article was unable to be deleted'
-      end
+
+  def destroy
+    if @article.destroy
+      flash[:success] = 'Article has been deleted'
+      redirect_to articles_path
+    else
+      flash[:danger] = 'Article was unable to be deleted'
     end
   end
+
 
   private
   def article_params
     params.require(:article).permit(:title, :body)
+  end
+
+  def set_article
+    @article = Article.find(params[:id])
   end
 end
 
